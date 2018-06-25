3278a4f516f08edb123c95ec83ebeb6c8ef50a17
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2018-06-25 19:12:57 -0400

    completion reorg

--- .config/bash/completion.bash
+++ .config/bash/completion.bash
@@ -1,15 +1,5 @@
-# kubectl completion
-KUBECTL_COMPLETION="$HOME/.config/bash/kubectl-completion.bash"
-if [ -s $KUBECTL_COMPLETION ]; then
-    . $KUBECTL_COMPLETION
-else
-    kubectl completion bash > $KUBECTL_COMPLETION && . $KUBECTL_COMPLETION 
-fi
+COMPLETION_DIR="$HOME/.config/bash/completion.d"
 
-# minikube completion
-MINIKUBE_COMPLETION="$HOME/.config/bash/minikube-completion.bash"
-if [ -s $MINIKUBE_COMPLETION ]; then
-    . $MINIKUBE_COMPLETION
-else
-    minikube completion bash > $MINIKUBE_COMPLETION && . $MINIKUBE_COMPLETION 
-fi
+for f in $COMPLETION_DIR/*; do
+  [ -f "$f" ] && . "$f"
+done
