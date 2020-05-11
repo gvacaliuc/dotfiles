038e1f9662f3ec99500e9a2ea21f012c5a9c3f4c
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2020-05-11 09:59:03 -0500

    update to current desk setup

--- .screenlayout/desk.sh
+++ .screenlayout/desk.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Set Frame Buffer Size
-xrandr --fb 4040x2400
-
-# Set HDMI2
-xrandr --output HDMI-1 --mode 1280x1024 --scale 1.75x1.75 --pos 0x304 --rotate normal
-xrandr --output HDMI-2 --mode 1600x1200 --scale 1.5x1.5 --pos 2240x0 --rotate left
-xrandr --output eDP-1 --off
+xrandr --output eDP-1 --mode 3840x2160
+xrandr --output DP-2 --mode 1600x1200 --scale 2x2 --right-of eDP-1
 
 # move odd monitors to the left, even to the right
-i3_left_right_split HDMI-1 HDMI-2
-
+i3_left_right_split eDP-1 DP-2
 
 # disable touchscreen and touchpad
-xinput list | grep -e "Touchscreen\s\+id" | grep -oe "id=[0-9]\+" | cut -d= -f2 | xargs xinput disable
-xinput list | grep -e "Synaptics TouchPad\s\+id" | grep -oe "id=[0-9]\+" | cut -d= -f2 | xargs xinput disable
+#xinput list | grep -e "Touchscreen\s\+id" | grep -oe "id=[0-9]\+" | cut -d= -f2 | xargs xinput disable
+#xinput list | grep -e "Synaptics TouchPad\s\+id" | grep -oe "id=[0-9]\+" | cut -d= -f2 | xargs xinput disable
