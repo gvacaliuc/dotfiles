f66667dbc149948e6fcc4539da241e2c07f26859
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2020-05-11 13:11:21 -0500

    updated to loop until it's killed tizonia daemon

--- .config/rofi/scripts/spotify-albums.sh
+++ .config/rofi/scripts/spotify-albums.sh
@@ -7,9 +7,19 @@ else
     # selected album / artist pairing will be provided as input
     album="$1"
 
-    # kill the existing daemon
-    ps aux | grep [t]izonia | cut -d" " -f2 | xargs kill -9
+    count=0
+    while [[ $(ps aux | grep [t]izonia) ]] && [ "$count" -lt "10" ]; do
+        # kill the existing daemon
+        ps aux | grep [t]izonia | cut -d" " -f2 | xargs kill -9
+        count=$((count+1))
+    done
+
+    [ "$count" -eq 10 ] && \
+        notify-send --urgency=low \
+        "failed to kill tizonia daemon, please kill manually" && \
+        exit
 
     # start new daemon with album search
-    tizonia --spotify-album "$album" --daemon
+    tizonia --spotify-album "$album" --daemon && \
+        notify-send --urgency=low "now playing: $album"
 fi 
